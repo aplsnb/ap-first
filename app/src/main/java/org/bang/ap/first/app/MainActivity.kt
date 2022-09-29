f8db2694e37946835c691a951f13e524d4dc3d2e
pujian <pujian.pj@foxmail.com>
2022-09-29 15:00:38 +0800

    materialButton和materialButtonToggleGroup控件

--- app/src/main/java/org/bang/ap/first/app/MainActivity.kt
+++ app/src/main/java/org/bang/ap/first/app/MainActivity.kt
@@ -2,6 +2,7 @@ package org.bang.ap.first.app
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.button.MaterialButtonToggleGroup
 import org.bang.ap.first.app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,33 +20,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        val navView: BottomNavigationView = binding.navView
+//
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navView.setupWithNavController(navController)
 
-        val navView: BottomNavigationView = binding.navView
+//        println("where is me ?")
+//
+//        R.id.text_home.onClick(this) {
+//            println("text_home onClick")
+//        }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        println("where is me ?")
-
-        R.id.text_home.onClick(this) {
-            println("text_home onClick")
+        setContentView(R.layout.comps_material_button)
+        val toggleGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggle_group)
+        toggleGroup.addOnButtonCheckedListener { _, _, _ ->
+            Log.e("MainActivity", "isChecked")
         }
     }
 }
 
-fun Int.onClick(activity: Activity, click: () -> Unit) {
-    activity.findViewById<View>(this).setOnClickListener {
-        println("onClick")
-        click()
-    }
-}
+//fun Int.onClick(activity: Activity, click: () -> Unit) {
+//    activity.findViewById<View>(this).setOnClickListener {
+//        println("onClick")
+//        click()
+//    }
+//}
